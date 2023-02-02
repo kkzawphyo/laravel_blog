bbfcc186ecde870181149868fdff2159c62b9502
waiyan <pmzawphyo1@gmail.com>
2023-02-02 16:58:29 +0900

    laravel_blog

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Article;
+use App\Models\Category;
+use App\Models\Comment;
+use App\Models\User;
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Article::factory()->count(20)->create();
+        Category::factory()->count(5)->create();
+        Comment::factory()->count(40)->create();
+
+        User::factory()->create([
+            "name" => "user",
+            "email" => "user@gmail.com",
+            ]);
+            User::factory()->create([
+            "name" => "admin",
+            "email" => "admin@gmail.com",
+            ]);
+    }
+}
